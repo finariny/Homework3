75eaef609e573622b645ee563462984d3f5c6337
anastasia <nastay.dr2002@gmail.com>
2022-12-01 18:34:46 +0400

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+        System.out.println("Task 1");
+        int myInt = 78;
+        System.out.println("Значение переменной myInt с типом int равно " + myInt);
+        byte myByte = 43;
+        System.out.println("Значение переменной myByte с типом byte равно " + myByte);
+        short myShort = 167;
+        System.out.println("Значение переменной myShort с типом short равно " + myShort);
+        long myLong = 17539L;
+        System.out.println("Значение переменной myLong с типом long равно " + myLong);
+        float myFloat = 134.0974F;
+        System.out.println("Значение переменной myFloat с типом float равно " + myFloat);
+        double myDouble = 9873.097;
+        System.out.println("Значение переменной myDouble с типом double равно " + myDouble);
     }
 }
